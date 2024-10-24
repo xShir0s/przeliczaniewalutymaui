a0e515fe67d7897fc2a71eca7b4f9018020f74fa
xShir0s <mtodys2006@gmail.com>
2024-10-24 09:35:52 +0200

    dodanie plikow

--- kantorwymianywalutzlodzieje/MainPage.xaml.cs
+++ kantorwymianywalutzlodzieje/MainPage.xaml.cs
@@ -1,8 +1,29 @@
-﻿namespace kantorwymianywalutzlodzieje
+﻿using System.Net;
+using System.Text.Json;
+
+namespace kantorwymianywalutzlodzieje
 {
+
+    public class Currency
+        {
+        public string?table{ get; set; }
+        public string? currency { get; set; }
+        public string? code { get; set; }
+        public IList<Rate> rates { get; set; }
+    }
+    public class Rate
+    {
+        public string? no { get; set; }
+        public string? effectiveDate { get; set; }
+        public double? bid { get; set; }
+        public double? ask { get; set; }
+    }
+
+
+
     public partial class MainPage : ContentPage
     {
-        int count = 0;
+       
 
         public MainPage()
         {
@@ -11,14 +32,28 @@
 
         private void OnCounterClicked(object sender, EventArgs e)
         {
-            count++;
+            string url = "https://api.nbp.pl/api/exchangerates/rates/c/usd/2024-10-22/?format=json";
+            string json;
+                using (var webClient = new WebClient())
+            {
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
+                json = webClient.DownloadString(url);
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+
+            }
+
+            Currency c = JsonSerializer.Deserialize<Currency>(json);
+
+            string s = $"nazwa waluty: {c.currency}\n";
+            s += $"Kod waluty: {c.code}\n";
+            s += $"Data: {c.rates[0].effectiveDate}\n";
+            s += $"Cena skupu: {c.rates[0].bid}\n";
+            s += $"Cena sprzedaży: {c.rates[0].ask}\n";
+            lblJSON.text = s;
+
+            
+
+
         }
     }
 
